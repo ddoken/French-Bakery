fb0c3e8e1936419ffeab5f976c13b797d3f9d38d
Defne Doken <ddoken24@Gmail.com>
2022-02-20 12:08:58 -0500

    Summary

---
+++ bakerygame/Assets/esc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esc : MonoBehaviour
+{
+    public bool IsPaused;
+    public GameObject Canvas;
+
+    void Update ()
+    {
+        if (IsPaused)
+        {
+            Canvas.SetActive(true);
+
+        }
+        else
+            Canvas.SetActive(false);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        { IsPaused = !IsPaused;
+        }
+        
+    }
+}
+  
